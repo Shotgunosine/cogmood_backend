fa04cdd805b1b1ad1978f5c74cc8e95a85b54723
Dylan Dellett-Wion <iamdylandw@gmail.com>
2024-10-30 09:33:05 -0400

    Initial addition of executable editing utils

--- app/taskstart.py
+++ app/taskstart.py
@@ -12,7 +12,7 @@ from flask import (
 from .io import write_metadata, initialize_taskdata
 from .config import CFG
 from .routing import routing
-from .utils import make_download
+from .utils import edit_exe_worker_id, edit_app_worker_id
 from hashlib import blake2b
 
 
@@ -29,8 +29,10 @@ def taskstart():
     supreme_subid = current_app.config['SUPREME_serializer'].dumps(h_workerId)
     win_dlpath = os.path.join(CFG['download'], 'win_' + str(session['subId']) + '.exe')
     mac_dlpath = os.path.join(CFG['download'], 'mac_' + str(session['subId']) + '.app')
-    make_download(supreme_subid, win_dlpath, 'windows')
-    make_download(supreme_subid, mac_dlpath, 'mac')
+    # make_download(supreme_subid, win_dlpath, 'windows')
+    edit_exe_worker_id(exe_file_path=BASE_EXE_PATH, new_worker_id=supreme_subid, output_file_path=win_dlpath)
+    # make_download(supreme_subid, mac_dlpath, 'mac')
+    edit_app_worker_id(app_path=BASE_APP_PATH, new_worker_id=supreme_subid, output_app_path=mac_dlpath)
     session['dlready'] = True
     write_metadata(session, ['dlready'], 'a')
     initialize_taskdata(session)
