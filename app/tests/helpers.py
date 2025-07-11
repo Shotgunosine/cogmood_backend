f198573e3178b57c0ab9be7221d0dc22079ed9fe
Dylan Nielson <adenosine@gmail.com>
2025-07-10 20:38:30 -0400

    add pytest prefix to workerid

--- app/tests/helpers.py
+++ app/tests/helpers.py
@@ -6,7 +6,7 @@ from ..config import CFG
 def get_new_workerid():
     newid = False
     while not newid:
-        workerId = str(random.randint(1, 1000000))
+        workerId = 'pytest_' + str(random.randint(1, 1000000))
         h_workerId = blake2b(workerId.encode(), digest_size=24).hexdigest()
         if h_workerId in os.listdir(CFG['meta']):
             continue
